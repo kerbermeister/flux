e212246de8a65a870ad0f5b0d43a3103661f73b0
Тарасов Антон Николаевич <tarasov_an@sptnk.co>
2021-11-30 15:07:43 +0300

    initial

---
+++ src/test/java/com/kerbermeister/mdc/MdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerbermeister.mdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
